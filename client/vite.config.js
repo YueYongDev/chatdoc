35ca507b899a839766adcd49e11a99fed18527b8
yueyong.lyy <yueyong.lyy@alibaba-inc.com>
2024-02-07 15:48:12 +0800

    update interface

--- client/vite.config.js
+++ client/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         // target: "http://127.0.0.1:8000",
-        target: "chatdoc.ytools.xyz",
+        target: "http://60.204.218.131:8000",
         changeOrigin: true,
         rewrite: path => path.replace(RegExp("/api"), ''),
       }
