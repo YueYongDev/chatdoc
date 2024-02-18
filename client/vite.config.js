a2a9565784f012570daf7ea4d51f221ddec6c249
yueyong.lyy <yueyong.lyy@alibaba-inc.com>
2024-02-18 21:55:37 +0800

    update 一下

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     server: {
         proxy: {
             "/api": {
-                target: "http://127.0.0.1:8999",
+                target: "http://60.204.218.131:8999",
                 changeOrigin: true,
                 ws: true,
                 rewrite: path => path.replace(RegExp("/api"), ''),
