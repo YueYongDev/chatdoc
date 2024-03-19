651bc326688e1ec5301ee082259b8caa595e6afc
yueyong.lyy <yueyong.lyy@alibaba-inc.com>
2024-03-19 09:53:49 +0800

    add new ui

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+    plugins: [react()],
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://60.204.218.131:8999",
+                changeOrigin: true,
+                ws: true,
+                rewrite: path => path.replace(RegExp("/api"), ''),
+            }
+        }
+    }
 });
